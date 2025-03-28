c69153f296bd39158c094468fb972261172a07e7
Bernard <bernarddira@gmail.com>
2025-03-28 17:34:24 +0200

    "add frontend files"

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+  return (
+    <>
+     <h1 className='bg-black-700'>Hello</h1>
+    </>
+  )
+
+export default App
