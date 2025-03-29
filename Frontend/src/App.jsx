f7bb39d313cf214a3a7ab971b45a01d8c6d9a4b6
Bernard <bernarddira@gmail.com>
2025-03-29 14:02:59 +0200

    "Add Frontend complete project structure"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+function App() {
   return (
-    <>
-     <h1 className='bg-black-700'>Hello</h1>
-    </>
+    <h1 className='bg-black-700'>Hello</h1>
   )
+}
 
 export default App
