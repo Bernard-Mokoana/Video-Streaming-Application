6b8a61d5ff13e507d91c7f607f4295955fd2391d
Bernard <bernarddira@gmail.com>
2025-04-05 17:42:42 +0200

    "set up Redux store"

---
+++ Frontend/src/app/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/auth/authSlice";
+import videoReducer from "../features/video/videoSlice";
+
+export const store = configureStore({
+    reducer: {
+        auth: authReducer,
+        video: videoReducer,
+    },
+
+    middleware: (getDefaultMiddleware) => {
+        getDefaultMiddleware( {
+            serializableCheck: false,
+        })
+    }
+});
