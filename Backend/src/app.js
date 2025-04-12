a7e71e4b9f572bfe3e563034e4113489184d1972
Bernard <bernarddira@gmail.com>
2025-04-12 17:10:42 +0200

    "update express app"

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,24 +4,24 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-// Enable CORS (Cross-Origin Resource Sharing)
+
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN, // Allow requests from the specified origin
-        credentials: true // Allow sending cookies with requests
+        origin: process.env.CORS_ORIGIN, 
+        credentials: true 
     })
 );
 
-// Common middleware
-app.use(express.json({ limit: "16kb" })); // Parse JSON requests with a size limit
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // Parse URL-encoded data
-app.use(express.static("public")); // Serve static files from the "public" directory
-app.use(cookieParser()); // Parse cookies from incoming requests
 
-// Import routes
-import healthcheckRouter from "./routes/healthcheck.routes.js"; // Health check route
-import userRouter from "./routes/user.routes.js"; // User-related routes
-import { errorHandler } from "./middleware/error.middlewares.js"; // Global error handler
+app.use(express.json({ limit: "16kb" })); 
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); 
+app.use(express.static("public")); 
+app.use(cookieParser()); 
+
+
+import healthcheckRouter from "./routes/healthcheck.routes.js"; 
+import userRouter from "./routes/user.routes.js"; 
+import { errorHandler } from "./middleware/error.middlewares.js"; 
 import tweetRouter from "./routes/tweet.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
@@ -30,9 +30,9 @@ import likeRouter from "./routes/like.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 
-// Define routes
-app.use("/api/v1/healthcheck", healthcheckRouter); // Health check endpoint
-app.use("/api/v1/users", userRouter); // User-related endpoints
+
+app.use("/api/v1/healthcheck", healthcheckRouter); 
+app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
@@ -41,7 +41,7 @@ app.use("/api/v1/like", likeRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
 
-// Global error handler middleware
+
 app.use(errorHandler);
 
-export { app }; // Export the app instance
+export { app }; 
