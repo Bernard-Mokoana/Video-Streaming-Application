576f840a2cb8fdd3ffde735cbc2b1eb17a718c37
Bernard <bernarddira@gmail.com>
2025-06-24 02:28:30 +0200

    "Restart the UI"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,7 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Box from "@mui/material/Box";
-import { store } from "./app/store.js";
-import { Provider } from "react-redux";
-import {
-  Navbar,
-  Feed,
-  ChannelDetails,
-  VideoDetails,
-  SearchFeed,
-  SideBar,
-} from "./components/index.js";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Box>
-          <Navbar />
-          <SideBar />
-          <Routes>
-            <Route path="/" exact element={<Feed />} />
-            <Route path="/video/:id" element={<VideoDetails />} />
-            <Route path="/channel/:id" exact element={<ChannelDetails />} />
-            <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
-          </Routes>
-        </Box>
-      </BrowserRouter>
-    </Provider>
-  );
+  return <div>App</div>;
 }
 
 export default App;
--- Frontend/src/components/common/Loader.jsx
+++
@@ -1,7 +0,0 @@
-const Loader = () => {
-    return (
-
-    )
-}
-
-export default Loader;
