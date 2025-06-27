357fada50b2e6365dfe330bb9badc69597ec7ae5
Bernard <bernarddira@gmail.com>
2025-06-27 14:39:48 +0200

    "Update UI and Add react routing"

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import Upload from "./Pages/Upload.jsx";
+import Watch from "./Pages/Watch.jsx";
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/upload" element={<Upload />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
