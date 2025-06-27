005de96cb0bd0f37929736aca5dea4e9eee26aaa
Bernard <bernarddira@gmail.com>
2025-06-27 04:49:26 +0200

    "Update the project"

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -3,18 +3,17 @@ import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
 const PORT = process.env.PORT || 8001;
 
-
 connectDB()
-.then(() => {
+  .then(() => {
     app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    })
-})
-.catch((err) => {
-    console.log("Mongodb connection error", err);
-})
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Server connection error", err);
+  });
